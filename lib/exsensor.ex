b570e8b55e972f574e6e318b851d4152389e28ff
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-03-22 16:48:37 +0100

    First commit

---
+++ lib/exsensor.ex
@@ -0,0 +1,18 @@
+defmodule Exsensor do
+  @moduledoc """
+  Documentation for Exsensor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exsensor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exsensor_test.exs
@@ -0,0 +1,8 @@
+defmodule ExsensorTest do
+  use ExUnit.Case
+  doctest Exsensor
+
+  test "greets the world" do
+    assert Exsensor.hello() == :world
+  end
+end
